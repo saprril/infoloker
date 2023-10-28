44c711279f96f151583b4cefcdfa1c871643ead7
saprril <safril463@gmail.com>
2023-10-29 06:18:26 +0700

    Added Profile Pages (can't click Save yet)

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,11 +3,17 @@ import { useState, useEffect } from "react";
 import Cookies from "universal-cookie";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+
+
 const cookies = new Cookies();
 export default function Profile() {
+    if (!cookies.get("TOKEN")) {
+        window.location.href = "/login";
+    }
     const id = cookies.get("USER");
     const token = cookies.get("TOKEN");
-    const [user, setUser] = useState({}); // Add an user object state
+    const [user, setUser] = useState({});
+    const [form, setForm] = useState({});
     //console.log(id)
     useEffect(() => {
         const fetchData = async () => {
@@ -15,6 +21,7 @@ export default function Profile() {
                 const response = await axios.get(`http://auth-server-sigma.vercel.app/users/profile/${id}`,
                 {headers: { Authorization: `Bearer ${token}` }});
                 setUser(response.data.user);
+                setForm(response.data.user);
             } catch (error) {
                 console.error("Error fetching user:", error);
             }
@@ -22,8 +29,74 @@ export default function Profile() {
         fetchData();
     },
     [id, token]);
-    console.log(user);
+    //console.log(user);
     const navigate = useNavigate();
+
+    const handleNameChange = (e) => {
+        const { name, value } = e.target;
+        //console.log([name]);
+        setForm({
+            ...form,
+            name: value,
+        });
+    };
+
+    const handleBirthplaceChange = (e) => {
+        const { value } = e.target;
+        //console.log([name]);
+        setForm({
+            ...form,
+            birthplace: value,
+        });
+    };
+    const handleGenderChange = (e) => {
+        //const { value } = e.target;
+        //console.log(e);
+        setForm({
+            ...form,
+            gender: e,
+        });
+    };
+    const handleBirthdayChange = (e) => {
+        //const { value } = e.target;
+        //console.log("e adalah", e.target);
+        const { value } = e.target;
+        setForm({
+            ...form,
+            birthday: value,
+        });
+    };
+    const handleAddressChange = (e) => {
+        const { value } = e.target;
+        setForm({
+            ...form,
+            address: value,
+        });
+    };
+    const handleCityChange = (e) => {
+        const { value } = e.target;
+        setForm({
+            ...form,
+            city: value,
+        });
+    };
+    const handleIdNumberChange = (e) => {   
+        const { value } = e.target;
+
+        setForm({
+            ...form,
+            idNumber: value,
+        });
+    };
+    const handleEducationChange = (e) => {
+        setForm({
+            ...form,
+            education: e,
+        });
+    };
+
+    //console.log(form);
+    console.log("Form", form);
     return (
         <div className="flex flex-col items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
         <a className="mb-5">
@@ -62,27 +135,33 @@ export default function Profile() {
                             labelProps={{
                                 className: "before:content-none after:content-none",
                             }}
+                            name="name"
+                            onChange={handleNameChange}    
+                            value={form.name}               
                         />
 
                         <Typography variant="h6" color="blue-gray" className="-mb-3">
                             Email
                         </Typography>
                         <Input
+                            disabled
                             size="lg"
                             placeholder="Your Email"
                             className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
                             labelProps={{
                                 className: "before:content-none after:content-none",
                             }}
+                            value={form.email}
+                            //onChange={handleInputChange}
                         />
 
                         <Typography variant="h6" color="blue-gray" className="-mb-3">
                             Gender
                         </Typography>
-                        <Select label="Select Version" size="lg">
-                            <Option> -- Pilih Jenis Kelamin --</Option>
-                            <Option>Pria</Option>
-                            <Option>Wanita</Option>
+                        <Select label="Select Version" size="lg" name="gender" value={form.gender} onChange={handleGenderChange}>
+                            <Option value=""> -- Pilih Jenis Kelamin --</Option>
+                            <Option value="Pria">Pria</Option>
+                            <Option value="Wanita">Wanita</Option>
                         </Select>
 
                         <Typography variant="h6" color="blue-gray" className="-mb-3">
@@ -95,6 +174,9 @@ export default function Profile() {
                             labelProps={{
                                 className: "before:content-none after:content-none",
                             }}
+                            onChange={handleBirthplaceChange}
+                            name="birthplace"
+                            value={form.birthplace}
                         />
 
                         <Typography variant="h6" color="blue-gray" className="-mb-3">
@@ -104,6 +186,9 @@ export default function Profile() {
                             type="date"
                             size="lg"
                             className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                            name="birthday"
+                            value={form.birthday}
+                            onChange={handleBirthdayChange}
                         />
 
                         <Typography variant="h6" color="blue-gray" className="-mb-3">
@@ -116,6 +201,9 @@ export default function Profile() {
                             labelProps={{
                                 className: "before:content-none after:content-none",
                             }}
+                            name="address"
+                            onChange={handleAddressChange}
+                            value={form.address}
                         />
 
                         <Typography variant="h6" color="blue-gray" className="-mb-3">
@@ -128,31 +216,36 @@ export default function Profile() {
                             labelProps={{
                                 className: "before:content-none after:content-none",
                             }}
+                            name="city"
+                            onChange={handleCityChange}
+                            value={form.city}
                         />
 
                         <Typography variant="h6" color="blue-gray" className="-mb-3">
                             ID Number
                         </Typography>
                         <Input
-                            type="number"
+                            type="text"
                             size="lg"
                             placeholder="ID Number"
                             className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
                             labelProps={{
                                 className: "before:content-none after:content-none",
                             }}
+                            onChange={handleIdNumberChange}
+                            value={form.idNumber}
                         />
 
                         <Typography variant="h6" color="blue-gray" className="-mb-3">
                             Education Level
                         </Typography>
-                        <Select label="Select Version" size="lg">
-                            <Option> -- Pilih Tingkat Pendidikan --</Option>
-                            <Option>SMA</Option>
-                            <Option>SMK</Option>
-                            <Option>D3</Option>
-                            <Option>S1</Option>
-                            <Option>S2</Option>
+                        <Select label="Select Version" size="lg" name="education" value={form.education} onChange={handleEducationChange}>
+                            <Option value=""> -- Pilih Tingkat Pendidikan --</Option>
+                            <Option value="SMA">SMA</Option>
+                            <Option value="SMK">SMK</Option>
+                            <Option value="D3">D3</Option>
+                            <Option value="S1">S1</Option>
+                            <Option value="S2">S2</Option>
                         </Select>
                     </div>
                     <Button className="mt-6" fullWidth>
