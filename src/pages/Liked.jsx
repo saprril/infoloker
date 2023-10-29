ba3143b5a0c66658972a40392a165adc79e6022a
saprril <safril463@gmail.com>
2023-10-29 07:34:13 +0700

    Added liked pages

--- src/pages/Liked.jsx
+++ src/pages/Liked.jsx
@@ -8,14 +8,44 @@ import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { Card, Input, Button, Typography } from "@material-tailwind/react";
 import { CardList } from "../components/CardList";
 import { Pagination } from "../components/Pagination";
+import Cookies from "universal-cookie";
+import { useEffect } from "react";
+import {Spinner} from "@material-tailwind/react";
+import axios from "axios";
+
+const cookies = new Cookies();
 
 export function Liked() {
   const [currentPage, setCurrentPage] = useState(1);
-  const [postPerPage, setPostPerPage] = useState(9);
+  const [isLoading, setIsLoading] = useState(true); // Add a loading state
+  const [likedJobs, setLikedJobs] = useState([]);
+  //const [likedCount, setLikedCount] = useState(0);
 
-  const lastPostIndex = currentPage * postPerPage;
-  const firstPostIndex = lastPostIndex - postPerPage;
-  const currentPosts = jobs.slice(firstPostIndex, lastPostIndex);
+  const token = cookies.get("TOKEN");
+  const id = cookies.get("USER");
+
+  if (!token) {
+    window.location.href = "/login";
+  }
+  useEffect(() => {
+    // Send the query parameter
+    setIsLoading(true);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://auth-server-sigma.vercel.app/users/liked/${id}`,
+          { headers: { Authorization: `Bearer ${token}`, } ,
+            params: {currentPage}});
+        setLikedJobs(response.data.jobs);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching user:", error);
+      }
+    };
+    fetchData();
+  }, [currentPage]);
+  //const lastPostIndex = currentPage * postPerPage;
+  //const firstPostIndex = lastPostIndex - postPerPage;
+  //const currentPosts = jobs.slice(firstPostIndex, lastPostIndex);
 
   return (
     <>
@@ -23,12 +53,21 @@ export function Liked() {
         
 
         <div>
-          <CardList page={currentPosts} />
+          {isLoading ? (
+            <div className="mx-auto flex items-center justify-center">
+              <Spinner />
+            </div> // Check loading state
+          ) : (
+            <>
+              <CardList page={likedJobs} />
+            </>
+          )}
           <div className="mx-auto max-w-screen-xl px-80 my-8 flex justify-center">
             <Pagination
-              totalPosts={jobs.length}
-              postPerPage={postPerPage}
+              totalPosts={likedJobs.length}
+              postPerPage={9}
               setCurrentPage={setCurrentPage}
+              currentPage={currentPage}
             />
           </div>
         </div>
